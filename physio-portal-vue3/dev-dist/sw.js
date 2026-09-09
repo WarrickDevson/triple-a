90946968c9a4558c2aef5843cabf219bfb0bd0d9
SidzingaDevson <sidzinga@devson.co.za>
2026-09-09 13:03:19 +0200

    App release

--- physio-portal-vue3/dev-dist/sw.js
+++ physio-portal-vue3/dev-dist/sw.js
@@ -78,7 +78,7 @@ define(['./workbox-afac4cd2'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.rdk6r1je93"
+    "revision": "0.37c9lvernlo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
